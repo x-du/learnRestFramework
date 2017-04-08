a6357bc6e6fa4a0ca83a49b75070b058a70cb70f
x_du <duxiaochen@yahoo.com>
2017-04-08 10:09:44 -0400

    model

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,7 +2,19 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    # owner = serializers.ReadOnlyField(source='owner.username')
+    # highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('url', 'id',
+                  'title', 'code', 'linenos', 'language', 'style')
+
+
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+#
+#     class Meta:
+#         model = User
+#         fields = ('url', 'id', 'username', 'snippets')
